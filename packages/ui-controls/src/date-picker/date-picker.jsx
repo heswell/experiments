d6f081379709c94fe45c07e4be9756f771c17faa
Steve Heron <steve@heswell.com>
2020-02-19 10:57:38 +0000

    functional DatePicker

---
+++ packages/ui-controls/src/date-picker/date-picker.jsx
@@ -0,0 +1,86 @@
+import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react';
+import {format, parseISO} from 'date-fns';
+import Calendar from '../inform/controls/calendar';
+import Selector,  {ComponentType} from '../inform/controls/selector/selector';
+
+import './date-picker.css';
+
+const YYYY_DD_MM = 'yyyy-MM-dd';
+const formatDate = date => format(date, YYYY_DD_MM);
+
+const defaultValues = [
+  "2018-12-18",
+  "2018-12-19",
+  "2018-12-20",
+  "2018-12-21",
+  "2018-12-22",
+  "2018-12-23",
+  "2018-12-24"
+];
+
+export default forwardRef(function DatePicker(props, ref){
+
+  const {
+    availableValues = defaultValues,
+    onCommit,
+    value
+  } = props;
+
+  const selector = useRef(null);
+  const [state, setState] = useState({values: availableValues, value});
+
+  useEffect(() => {
+    if (value !== state.value){
+      setState({...state, value })
+    }
+  },[value]);
+
+  useImperativeHandle(ref, () => ({ focus }))
+
+  function focus(){
+    if (selector.current){
+      selector.current.focus(false)
+    }
+  }
+
+  const handleCommit = value => onCommit(formatDate(parseISO(value)));
+  
+  const handleChange = e => {
+    const value = e.target.value;
+    setState({ value, values: matchingValues(value) });
+  }
+
+  function matchingValues(text){
+    const pattern = new RegExp(`^${text}`,'i')
+    return availableValues.filter(value => pattern.test(value))
+  }
+
+  return (
+    <Selector ref={selector}
+      {...props}
+      value={state.value}
+      availableValues={state.values}
+      onChange={handleChange}
+      onCommit={handleCommit}
+      valueFormatter={formatDate}
+      inputClassName="date-input"
+      inputIcon="date_range"
+      dropdownClassName="date-picker-dropdown"
+      showDropdownOnEdit={false}
+    >
+    {
+      child =>
+        child === ComponentType.Dropdown && (
+          <Calendar
+            value={state.value}>
+            {formattedDate =>
+              <span className="calendar-day">{formattedDate}</span>
+            }
+          </Calendar>
+        )
+
+    }
+    </Selector>
+  )
+
+})
