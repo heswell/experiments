03053fd52a35745c76ab4a7a7a3403289ac36d66
Steve Heron <steve@heswell.com>
2020-02-19 13:14:32 +0000

    functional composite

--- packages/ui-controls/src/inform/controls/composite-control/composite-control.jsx
+++ packages/ui-controls/src/inform/controls/composite-control/composite-control.jsx
@@ -1,69 +1,63 @@
-import React from 'react';
+import React, {forwardRef, useImperativeHandle, useRef, useState } from 'react';
 
 import './composite-control.css';
 
-export default class CompositeControl extends React.Component {
-  constructor(props){
-    super(props);
-    this.childRefs = [];
-    this.state = {
-      value: props.children.map(child => child.props.value)
-    }
-  }
+export default forwardRef(function CompositeControl({
+  children: nestedControls,
+  field,
+  onCancel,
+  onCommit,
+  onFocus,
+  onPopupActive
+}, ref){
 
-  focus(idx=0){
-    const component = this.childRefs[idx];
+  const [value, setValue] = useState(nestedControls.map(child => child.props.value));
+  const childRefs = useRef([]);
+
+  useImperativeHandle(ref, () => ({ focus }));
+
+  function focus(idx=0){
+    const component = childRefs.current[idx];
     if (component && component.focus){
-      console.log(`composite-control focus ${idx}`)
       component.focus();
     }
   }
 
-  setRef(c, idx){
-    this.childRefs[idx] = c;
-  }
+  const setRef = (c, idx) => childRefs.current[idx] = c;
 
-  commit(newValue, idx, controlCommitCallback){
-    const {field} = this.props;
+  function handleCommit(newValue, idx, controlCommitCallback){
     console.log(`composite (${field.id}) commit ${idx}`)
-    const value = [...this.state.value];
-    value[idx] = newValue;
-    this.setState({value},() => {
-      // controlCommitCallback(this.state.value[idx])
-      controlCommitCallback(this.state.value)
-    })
-    this.props.onCommit(field);
+    const newValues = [...value];
+    newValues[idx] = newValue;
+    setValue(newValues);
+    controlCommitCallback(newValues);
+    onCommit(field);
   }
 
-  render(){
-    const {children, onCancel, onFocus, onPopupActive} = this.props;
-    return (
-      <div className="composite-control">
-      {children.map((child,idx) => {
-          const props = {
-            ref: c => this.setRef(c, idx),
-            onFocus: () => {
-              console.log(`[composite-control] onFocus [${idx}]`)
-              onFocus(idx)
-            },
-            onPopupActive,
-            onCancel
+  return (
+    <div className="composite-control">
+    {nestedControls.map((child,idx) => {
+        const props = {
+          ref: c => setRef(c, idx),
+          onFocus: () => onFocus(idx),
+          onPopupActive,
+          onCancel
+        }
+        if (child.props.onCommit){
+          const _commit = child.props.onCommit;
+          props.onCommit = value => {
+            handleCommit(value, idx, _commit)
           }
-          if (child.props.onCommit){
-            const _commit = child.props.onCommit;
-            props.onCommit = value => {
-              this.commit(value, idx, _commit)
-            }
-          }
-          const component = React.cloneElement(child, props)
+        } // suppose it doesn't have commit, shouldn't we commit anyway ?
+        const component = React.cloneElement(child, props)
 
-        return (
-          <div className="composite-item" key={idx}>
-            {component}
-          </div>
-        )
-        })}
+      return (
+        <div className="composite-item" key={idx}>
+          {component}
+        </div>
+      )
+      })}
     </div>
-    )
-  }
-}
+  )
+
+});
