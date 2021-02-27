eb201d74c3922b1822bd831cdaf3ac28eb3ab225
Steve Heron <steve@heswell.com>
2021-02-27 19:43:06 +0000

    Publish
     - @heswell/layout@3.0.15

--- packages/layout/src/responsive/index.js
+++ packages/layout/src/responsive/index.js
@@ -1,5 +1,4 @@
-export { default as Measure } from './Measure';
-export { default as OverflowMenu } from './OverflowMenu';
+// export { default as OverflowMenu } from './OverflowMenu';
 // export { default as useBreakpoints } from './useBreakPoints';
 export { default as useOverflowObserver } from './useOverflowObserver';
 export { default as useResizeObserver } from './useResizeObserver';
